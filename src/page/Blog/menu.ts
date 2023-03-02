943ec505883da0bc94fff92027d489f541c54073
Cunjun Yin <cunjunyin@outlook.com>
2023-03-02 21:54:45 +1000

    remove images

--- src/page/Blog/menu.ts
+++ src/page/Blog/menu.ts
@@ -76,6 +76,225 @@ export var menu: MenuType =
 			]
 		},
 		{
+			title: "Design Patterns",
+			haveBlogs: false,
+			hasChildren: true,
+			children: [
+				{
+					title: "Behavioral Design Patterns",
+					haveBlogs: true,
+					hasChildren: true,
+					children: [
+						{
+							title: "Chain Of Responsibility",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/01.ChainOfResponsibility.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Command",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/02.Command.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Iterator",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/03.Iterator.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Mediator",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/04.Mediator.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Memento",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/05.Memento.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Observer",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/06.Observer.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "State",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/07.State.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Strategy",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/08.Strategy.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Template Method",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/09.TemplateMethod.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Visitor",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Behavioral%20Design%20Patterns/10.Visitor.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+					]
+				},
+				{
+					title: "Creational Patterns",
+					haveBlogs: true,
+					hasChildren: true,
+					children: [
+						{
+							title: "Factory Method",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Creational%20Patterns/01.FactoryMethod.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Abstract Factory",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Creational%20Patterns/02.AbstractFactory.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Builder",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Creational%20Patterns/03.Builder.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Prototype",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Creational%20Patterns/04.Prototype.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Singleton",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Creational%20Patterns/05.Singleton.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+					]
+				},
+				{
+					title: "Structural Design Patterns",
+					haveBlogs: true,
+					hasChildren: true,
+					children: [
+						{
+							title: "Adapter",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/01.Adapter.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Bridge",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/02.Bridge.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Composite",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/03.Composite.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Decorator",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/04.Decorator.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Facade",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/05.Facade.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Flyweight",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/06.Flyweight.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+						{
+							title: "Proxy",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/Structural%20Design%20Patterns/07.Proxy.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+					]
+				},
+				{
+					title: "Other",
+					haveBlogs: true,
+					hasChildren: true,
+					children: [
+						{
+							title: "Dependency Injection",
+							uri: "https://raw.githubusercontent.com/CunjunYin/notes/main/DesignPatterns/01.DependencyInjection.md",
+							excerpt: "",
+							background: "#EABA6B",
+							hasChildren: false,
+							children: []
+						},
+					]
+				}
+			]
+		},
+		{
 			title: "Protocols",
 			haveBlogs: false,
 			hasChildren: true,
